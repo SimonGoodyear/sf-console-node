1a0f30730db8e901bcb5ba3b41f9a7c542873663
Simon Goodyear <simon.goodyear@sgopus.com>
2011-07-04 23:13:06 +0100

    Updated for heroku port madness

--- test.js
+++ test.js
@@ -4,16 +4,16 @@ res.writeHead( 200, {
     'Set-Cookie': 'mycookie=test',
     'Content-Type': 'text/plain'
   });
-res.write('hello again');
+res.write('hello again ' + process.env.PORT);
 res.end(JSON.stringify(req.cookies)); });
-//var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
 
-server.listen(80);
+server.listen(process.env.PORT);
 
-/*io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
-});*/
+});
 
