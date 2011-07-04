62356a98571e0b5f2b9d2bb212d9a2ca855a649f
Simon Goodyear <simon.goodyear@sgopus.com>
2011-07-04 23:02:12 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+var connect = require('connect');
+var server = connect.createServer( connect.cookieParser(), function(req,res){
+res.writeHead( 200, {
+    'Set-Cookie': 'mycookie=test',
+    'Content-Type': 'text/plain'
+  });
+res.write('hello again');
+res.end(JSON.stringify(req.cookies)); });
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
