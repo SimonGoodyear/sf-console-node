3545f013e39f3e93e2baf5281ea0f75a44c956b9
Simon Goodyear <simon.goodyear@sgopus.com>
2011-07-05 12:27:19 +0100

    err

--- test.js
+++ test.js
@@ -55,10 +55,10 @@ function HttpHandler(req,res){
             }
             catch (err) {
                 result = err.stack;
-                console.log(result);
+                console.log('err:   ' + result);
             }
             console.log('obj:  ' + obj);
-
+			console.log(result);
             res.write(result);
             res.end();
         });
