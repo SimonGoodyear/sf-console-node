361acb185b10198692567138f32e7b07567729c1
Simon Goodyear <simon.goodyear@sgopus.com>
2011-07-05 12:44:48 +0100

    closing tags

--- test.js
+++ test.js
@@ -49,17 +49,20 @@ function HttpHandler(req,res){
         req.on('end', function() {
             var xml = chunks.join(''), result;
             try {
-	            result = xml.replace('soapenv:Envelope ', 'soapenvEnvelope ')
+	            result = xml.replace('<soapenv:Envelope ', 'soapenvEnvelope ')
 	            			.replace(' xmlns:soapenv="', ' xmlnssoapenv="')
 	            			.replace(' xmlns:xsd="', ' xmlnsxsd="')
 	            			.replace(' xmlns:xsi="', ' xmlnsxsi="')
 	            			.replace(' xsi:type="', ' xsitype="') 
 	            			.replace(' xmlns:sf="', ' xmlnssf="')
 	            			.replace(' xsi:nil="', ' xsinil="')	            			
-	            			.replace('soapenv:Body>', 'soapenvBody>')
-	            			.replace('sf:Id>', 'sfId>')
-	            			.replace('sf:Message__c>', 'sfMessage__c>')
-	            			.replace('sf:Updated__c>', 'sfUpdated__c>')
+	            			.replace('<soapenv:Body>', '<soapenvBody>')
+	            			.replace('<sf:Id>', '<sfId>')
+	            			.replace('<sf:Message__c>', '<sfMessage__c>')
+	            			.replace('<sf:Updated__c>', '<sfUpdated__c>')	            			.replace('</soapenv:Body>', '</soapenvBody>')
+	            			.replace('</sf:Id>', '</sfId>')
+	            			.replace('</sf:Message__c>', '</sfMessage__c>')
+	            			.replace('</sf:Updated__c>', '</sfUpdated__c>')
 	            			.replace('<?xml version="1.0" encoding="UTF-8"?>','');
 	            			
                // result = parser.toJson(xml);
