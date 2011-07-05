c3f0aa075f3e50bbd8fbe0d5465edc6cb1fb7447
Simon Goodyear <simon.goodyear@sgopus.com>
2011-07-05 16:34:17 +0100

    s

--- test.js
+++ test.js
@@ -74,7 +74,7 @@ function HttpHandler(req,res){
                 console.log('err:   ' + result);
             }
             console.log('obj: ' + obj);
-            console.log('val1: ' + obj.soapenvBody);
+            console.log('val1: ' + obj.OrganizationId);
 			console.log(result);
             //res.write(result);
             res.end();
