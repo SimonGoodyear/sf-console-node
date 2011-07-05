f8dde103e137c64c8377445353ca3c14ce427e3d
Simon Goodyear <simon.goodyear@sgopus.com>
2011-07-05 12:24:19 +0100

    obj debug

--- test.js
+++ test.js
@@ -56,7 +56,7 @@ function HttpHandler(req,res){
             catch (err) {
                 result = err.stack;
             }
-            //console.log(obj.soapEnvelope);
+            console.log('obj:  ' + obj);
             res.write(result);
             res.end();
         });
